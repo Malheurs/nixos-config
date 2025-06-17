0ed918a6d92a236822161fde2d7ea57a83868713
Malheurs <malheurcornelis@proton.me>
2025-06-17 18:15:56 +0200

    Color gamut

--- modules/hyprland/monitors.nix
+++ modules/hyprland/monitors.nix
@@ -3,7 +3,7 @@
 {
   home.file.".config/hypr/monitors.conf".text = ''
     # CONFIGURATION MONITEURS
-    monitor = DP-2,3440x1440@165,1920x1440,1,vrr,3,bitdepth,10,cm,wide,sdrbrightness,1.0,sdrsaturation,1.0
+    monitor = DP-2,3440x1440@165,1920x1440,1,vrr,3,bitdepth,10,cm,auto,sdrbrightness,1.0,sdrsaturation,1.0
     monitor = DP-3,3440x1440@120,1920x0,1
     monitor = HDMI-A-1,1920x1080@60,0x1800,1
 
