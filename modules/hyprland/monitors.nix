9c32c0ab136ba1ae5db3f793e5853a349e779778
Malheurs <malheurcornelis@proton.me>
2025-06-17 17:35:22 +0200

    Hyprland HDR & back to old syntax

--- modules/hyprland/monitors.nix
+++ modules/hyprland/monitors.nix
@@ -3,28 +3,9 @@
 {
   home.file.".config/hypr/monitors.conf".text = ''
     # CONFIGURATION MONITEURS
-    monitorv2 {
-      output = DP-2
-      mode = 3440x1440@165
-      position = 1920x1440
-      scale = 1
-      vrr = 3
-      bitdepth = 10
-      cm = hdr
-      sdrbrightness = 1.0
-      sdrsaturation = 1.0
-      }
-    monitorv2 {
-      output = DP-3
-      mode = 3440x1440@120
-      position = 1920x0
-      scale = 1}
-    monitorv2 {
-      output = HDMI-A-1
-      mode = 1920x1080@60
-      position = 0x1800
-      scale = 1
-      }
+    monitor = DP-2,3440x1440@165,1920x1440,1,vrr,3,bitdepth,10,cm,hdr,sdrbrightness=1.0,sdrsaturation=1.0
+    monitor = DP-3,3440x1440@120,1920x0,1
+    monitor = HDMI-A-1,1920x1080@60,0x1800,1
 
     # ESPACES DE TRAVAIL
     workspace = 1,monitor:DP-2,persistent:true,default:true
