8237b15ab07e371ce4a8d8f0376d21b6cd93b0c2
Malheurs <malheurcornelis@proton.me>
2025-06-20 11:33:19 +0200

    Windowsrules Steam

--- modules/hyprland/windowrules.nix
+++ modules/hyprland/windowrules.nix
@@ -63,9 +63,12 @@
     windowrule = float,class:^([Ss]team)$
     windowrule = float,class:([Ss]team)$,title:^(Paramètres Steam|Steam Settings|Lancement...)$
     windowrule = center 1,class:^([Ss]team)$,title:^(Paramètres Steam|Steam Settings|Lancement...)$
-    windowrule = monitor DP-2,class:^steam_app_\\d+$
-    windowrule = workspace 4,class:^steam_app_\\d+$
-    windowrule = fullscreen,class:^steam_app_\\d+$
+    windowrule = monitor DP-2,class:^steam_app_[0-9]+$
+    windowrule = workspace 4,class:^steam_app_[0-9]+$
+    windowrule = fullscreen,class:^steam_app_[0-9]+$
+    windowrule = monitor DP-2,class:^steam_app_.*$
+    windowrule = workspace 4,class:^steam_app_.*$
+    windowrule = fullscreen,class:^steam_app_.*$
     windowrule = stayfocused,title:^()$,class:^([Ss]team)$
     windowrule = minsize 1 1,title:^()$,class:^([Ss]team)$
 
